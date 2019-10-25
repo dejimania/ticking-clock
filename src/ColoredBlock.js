40a6647d95ff838601b2345b9bd48b2dde71b9c0
Kamil Balogun <kamilbalogun1@gmail.com>
2019-10-25 21:52:49 +0100

    Add state to ColoredButton.js

--- src/ColoredBlock.js
+++ src/ColoredBlock.js
@@ -2,9 +2,15 @@ import React from 'react';
 import ChangeColorButton from './ChangeColorButton';
 
 class ColoredBlock extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            backgroundColor: 'red'
+        };
+    }
     render() {
         return (
-            <div style={{height: '200px', width: '200px', color: 'red'}}>
+            <div style={{height: '200px', width: '200px', backgroundColor: this.state.backgroundColor}}>
                 <ChangeColorButton></ChangeColorButton>
             </div>
         )
